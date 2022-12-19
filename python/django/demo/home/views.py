3b27dba6aed87756d20ca5d8f1abc905311d16d1
ngocson1603 <100844717+ngocson1603@users.noreply.github.com>
2022-12-19 17:28:54 +0700

    v1

--- python/django/demo/home/views.py
+++ python/django/demo/home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import department as department_model
 # Create your views here.
 
 
 def get_home(request):
-    return render(request, 'home.html')
+    department_list = department_model.objects.filter().order_by('department_id')
+    return render(request, 'home.html', {'department_list': department_list})
 
 
 def testing(request):
